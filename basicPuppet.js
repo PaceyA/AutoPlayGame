9c2fbb4e7fb71fb19f30230be6a50cc17421e3c5
Alex Pacey <alex.pacey@thamestechnology.co.uk>
2024-02-06 16:31:14 +0000

    Scores are written to a text file also

--- basicPuppet.js
+++ basicPuppet.js
@@ -21,9 +21,25 @@ async function run() {
     try{
       await page.waitForSelector('h1',{timeout:2000}) //Try to find the h1 element. This is fine because there is only 1 h1 tag and that is when the game over state is reached
 
+
+      //Get the score from the run
+      const score = await page.evaluate(() => {
+        return document.querySelector('div.score').textContent
+      })
+      
+      if(fs.existsSync('filename'))
+      {
+      //If the score text file doesnt exist then create one
+      }else{
+      //Save the score to the text file containing scores of previous runs
+      fs.appendFile('./Scores/values.txt', `${score}\n`, function(err){
+        if (err) throw err;
+      })}
+
+      //score group flex shrink-0 cursor-default items-center justify-end min-w-[200px] text-5xl px-4 py-2 gap-2 rounded-3xl font-bold text-white svelte-t2cjo8
       //If found then we take a screenshot of the score and then change the game over varaible so the loop ends
       const numOfFiles = fs.readdirSync('./Scores').length;
-      await page.screenshot({path: `./Scores/${numOfFiles}score.png`})
+      await page.screenshot({path: `./Scores/${numOfFiles-1}score.png`})
       gameOver = true;
       
     }catch(error)
